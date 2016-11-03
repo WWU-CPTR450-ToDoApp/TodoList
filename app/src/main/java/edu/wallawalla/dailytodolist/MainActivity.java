e0a23849945755dc632e8c28aa7b7dc16024f5f4
meablanche <meablanche@gmail.com>
2016-11-03 15:56:29 -0700

    testing new branch more

--- app/src/main/java/edu/wallawalla/dailytodolist/MainActivity.java
+++ app/src/main/java/edu/wallawalla/dailytodolist/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         Dictionary<Integer, String> d = new Hashtable<>();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
     }
 }
