ce3f341284d9eddf6859efa37ff0668d6745b2a5
CyberNinja42 <bobby.berglund@gmail.com>
2016-11-03 15:03:40 -0700

    Dictionary

--- app/src/main/java/edu/wallawalla/dailytodolist/MainActivity.java
+++ app/src/main/java/edu/wallawalla/dailytodolist/MainActivity.java
@@ -3,10 +3,14 @@ package edu.wallawalla.dailytodolist;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Dictionary;
+import java.util.Hashtable;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Dictionary<Integer, String> d = new Hashtable<Integer, String>();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
